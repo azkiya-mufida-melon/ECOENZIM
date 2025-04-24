4f614db146712583531add85396041bd20ef648f
LENOVO <nadia23@mhs.politala.ac.id>
2025-04-24 18:30:39 +0800

    tampilan profil

---
+++ app/src/main/java/com/example/pemantauanecoenzim/prof.java
@@ -0,0 +1,18 @@
+package com.example.pemantauanecoenzim;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+// Class tambahan sesuai permintaan
+class prof {
+    // Tambahkan isi class prof di sini sesuai kebutuhan
+}
